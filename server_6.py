bba859de325118e3143acfa25d77c7c2ea8cf4ac
Javith farkhan. S <94296805+Javith-farkhan@users.noreply.github.com>
2023-05-29 09:28:14 -0400

    Add files via upload

---
+++ server_6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import socket
+s=socket.socket()
+s.connect(('localhost',8000))
+while True:
+    ip=input("Enter the website you want to ping ")
+    s.send(ip.encode())
+print(s.recv(1024).decode())
+
